b8a48dc881c7df4935b81929e664963c15cade77
leozeleba44@gmail.com <leozeleba44@gmail.com>
2020-12-13 19:09:28 +0100

    Navbar : Creation

---
+++ src/components/NavBar.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="nav-bar">
+        
+        <ul>
+            <li class="category" v-for="category in categories" :key="category.id">
+                <h2 class="category-name">{{ category.name }}</h2>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'NavBar',
+    data(){
+        return{
+            categories:[
+
+                {
+                    id: 1,
+                    name: "Profile",
+                    link: "/profile",
+                    svg: ""  
+                },
+                {
+                    id: 2,
+                    name: "Formations",
+                    link: "/courses",
+                    svg: ""  
+                },
+                {
+                    id: 3,
+                    name: "Experiences",
+                    link: "/experiences",
+                    svg: ""  
+                },
+                {
+                    id: 4,
+                    name: "Compétences",
+                    link: "/skills",
+                    svg: ""  
+                },
+                {
+                    id: 5,
+                    name: "Portfolio",
+                    link: "/portfolio",
+                    svg: ""  
+                },
+                {
+                    id: 6,
+                    name: "Hobbies",
+                    link: "/hobbies",
+                    svg: ""  
+                },
+                {
+                    id: 7,
+                    name: "Contact",
+                    link: "/contact",
+                    svg: ""  
+                }
+                
+            ]
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .nav-bar{
+        background-color: rgb(255, 255, 255);
+        border-right: grey solid 2px;
+        width: 23%;
+    }
+
+    .nav-bar > ul{
+        list-style-type: none;
+        
+    }
+
+    .category{
+        margin: 20px 0px;
+    }
+
+    .category-name{
+        text-transform: uppercase;
+    }
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="nav-bar">
+    <NavBar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.nav-bar{
+}
+</style>
