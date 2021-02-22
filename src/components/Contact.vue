e52f5a39a081a3d67b6f3cefe696d61b2d647e77
Léo Bories <leozeleba44@gmail.com>
2021-02-22 20:01:54 +0100

    Add assets and contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="contact">
+        <div class="content">
+            <p>mail : <a href="mailto:leobories42@gmail.com">leobories42@gmail.com</a></p>
+            <p>tel : 07.81.25.52.82</p>
+            <a href="https://www.linkedin.com/in/l%C3%A9o-bories-b6166b173?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B1irkmlWlT4SjALLo9S3ckQ%3D%3D">Linkedin</a>
+            <p>Ce site internet à été développé à partir du framework Vue.js et son code est disponnible sur github : <a href="https://github.com/leozeleba/my-web-site">https://github.com/leozeleba/my-web-site</a></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact'
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .contact{
+        background-color: #737373;
+        height: 7em
+    }
+    .content{
+        padding-left: 7em;
+        text-align: center;
+        padding-top: 1em
+    }
+</style>
